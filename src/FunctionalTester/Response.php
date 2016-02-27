828c3fac583180b23f51c85f4a59e73636cf526f
Tomy <kazushige.tominaga@mkt-apps.com>
2016-02-27 19:34:09 +0900

    add get cookie function

--- src/FunctionalTester/Response.php
+++ src/FunctionalTester/Response.php
@@ -2,6 +2,56 @@
 namespace FunctionalTester;
 
 use Guzzle\Http\Message\Response as GuzzleResponse;
+use Guzzle\Parser\ParserRegistry;
 
 class Response extends GuzzleResponse
-{}
+{
+    protected $cookies = null;
+
+    /**
+     * @param string $message
+     * @return self|bool
+     */
+    public static function fromMessage($message)
+    {
+        return parent::fromMessage($message);
+    }
+
+    /**
+     * @param $cookieStr
+     * @return mixed
+     */
+    protected function parseCookie($cookieStr)
+    {
+        return ParserRegistry::getInstance()->getParser('cookie')->parseCookie($cookieStr);
+    }
+
+    /**
+     * @param $cookieStrings
+     * @return array
+     */
+    protected function parseCookies($cookieStrings)
+    {
+        $cookies = [];
+        foreach ($cookieStrings as $cookieStr) {
+            $cookies[] = $this->parseCookie($cookieStr);
+        }
+
+        return $cookies;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getCookies()
+    {
+        if (!$this->cookies && $this->getSetCookie()) {
+            $cookieStrings = $this->getHeader('Set-Cookie')->toArray();
+            $this->cookies = $this->parseCookies($cookieStrings);
+
+            return $this->cookies;
+        }
+
+        return null;
+    }
+}
---
+++ tests/FunctionalTester/ResponseTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace Tests\Response;
+
+use Tests\lib\TestCase;
+use FunctionalTester\Response;
+
+class ResponseTest extends TestCase
+{
+    function test_getCookies()
+    {
+        $this->specify('if Set-cookie is unspecified', function () {
+            $message = file_get_contents(__DIR__ . '/data/mock_response');
+            $response = Response::fromMessage($message);
+
+            $this->assertNull($response->getCookies());
+        });
+
+        $this->specify('if Set-cookies header is specified', function () {
+            $message = file_get_contents(__DIR__ . '/data/mock_response_multiple_cookies');
+            $response = Response::fromMessage($message);
+
+            $this->assertEquals($response->getCookies(), [
+                [
+                    'domain' => 'hogehoge.com',
+                    'path' => '/hogehoge',
+                    'max_age' => '202054542594',
+                    'expires' => 'Fri, 04-Jan-8419 16:11:51 GMT',
+                    'version' => null,
+                    'secure' => null,
+                    'port' => null,
+                    'discard' => false,
+                    'comment' => null,
+                    'comment_url' => null,
+                    'http_only' => false,
+                    'cookies' => [
+                        'hoge' => 'hogehoge'
+                    ],
+                    'data' => [],
+                ],
+                [
+                    'domain' => 'fugafuga.com',
+                    'path' => '/fugafuga',
+                    'max_age' => '202054542594',
+                    'expires' => 'Fri, 04-Jan-8419 16:11:51 GMT',
+                    'version' => null,
+                    'secure' => null,
+                    'port' => null,
+                    'discard' => false,
+                    'comment' => null,
+                    'comment_url' => null,
+                    'http_only' => false,
+                    'cookies' => [
+                        'fuga' => 'fugafuga'
+                    ],
+                    'data' => [],
+                ],
+            ]);
+        });
+    }
+}
