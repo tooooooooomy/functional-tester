61c66ff9df087940a1fbe117c39e35798216cddf
yowcow <yowcow@cpan.org>
2016-02-28 02:30:00 +0000

    dump stdout in travis...

--- tests/FunctionalTester/RequestTest.php
+++ tests/FunctionalTester/RequestTest.php
@@ -181,6 +181,8 @@ END
         $this->assertEquals(255, $ret);
         $this->assertTrue(strlen($stderr) > 0);
 
+        var_dump($stdout);
+
         preg_match('/\r\n\r\n(.+)$/', $stdout, $matches);
 
         $this->assertEquals($matches[1], 'hoge');
